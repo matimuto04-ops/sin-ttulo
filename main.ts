455437b12f3f2cb67e084349c7a8bbc239c701ab
matimuto04-ops <matimuto04@gmail.com>
2026-02-26 11:41:17 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    music.play(music.stringPlayable("G B A G C5 B A B ", 100), music.PlaybackMode.InBackground)
+})
+basic.forever(function () {
+    music.play(music.stringPlayable("C5 B A G F E D C ", 100), music.PlaybackMode.InBackground)
+})
+basic.forever(function () {
+    music.play(music.stringPlayable("F D B C5 B C5 D F ", 100), music.PlaybackMode.InBackground)
+})
